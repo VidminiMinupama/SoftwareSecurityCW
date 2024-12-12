4b3764219cf3a952a6340fd9cb1c3e4ffb6c3915
VidminiMinupama <vidminibatuwanthudawa@gmail.com>
2024-12-12 22:36:24 +0530

    first commit

---
+++ server/generateSecureKey.js
@@ -0,0 +1,9 @@
+import crypto from 'crypto';
+
+const generateSecureKey = () => {
+  return crypto.randomBytes(32).toString('hex');
+};
+
+console.log('PASSWORD_PEPPER=', generateSecureKey());
+console.log('JWT_SECRET=', generateSecureKey());
+console.log('TRANSIT_KEY=', generateSecureKey());
